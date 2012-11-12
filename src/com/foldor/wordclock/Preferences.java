361b0849260610e92dfbe38328ea3396579760a9
Ryan Myers <ryan.p.myers@gmail.com>
2012-11-11 22:52:58 -0500

    - Removed bin folder and ignored it - Made significant progress in getting the widget ready.

--- src/com/foldor/wordclock/Preferences.java
+++ src/com/foldor/wordclock/Preferences.java
@@ -39,7 +39,7 @@ public class Preferences extends PreferenceActivity {
 					result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
 					setResult(RESULT_OK, result);
 															
-					sendBroadcast(new Intent(android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE, Uri.EMPTY, this, WordClockReceiver.class));
+					sendBroadcast(new Intent(android.appwidget.AppWidgetManager.ACTION_APPWIDGET_ENABLED, Uri.EMPTY, this, WordClockReceiver.class));
 				}
 			}
 		}
--- src/com/foldor/wordclock/WordClockReceiver.java
+++ src/com/foldor/wordclock/WordClockReceiver.java
@@ -13,7 +13,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -22,7 +21,7 @@ public class WordClockReceiver extends AppWidgetProvider {
 	//Log Tag.
 	private static final String TAG = "WordClock";
 	
-    //Create new intents
+	//Create new intents
     private final static IntentFilter sIntentFilter;
     static {
         sIntentFilter = new IntentFilter();
@@ -30,17 +29,67 @@ public class WordClockReceiver extends AppWidgetProvider {
         sIntentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
         sIntentFilter.addAction(Intent.ACTION_TIME_CHANGED);
     }
-    
-    @Override
-    public void onEnabled(Context context) {
-        context.registerReceiver(mTimeChangedReceiver, sIntentFilter);
-    }
-    
-    @Override
-    public void onDisabled(Context context) {
-        context.unregisterReceiver(mTimeChangedReceiver);
-    }
 	
+	 @Override
+	 public void onEnabled(Context context) {
+		 Log.d(TAG, "Enabled");
+		 Context appContext = context.getApplicationContext();
+		 
+		 appContext.registerReceiver(mTimeChangedReceiver, sIntentFilter);
+		 
+		 AppWidgetManager manager = AppWidgetManager.getInstance(appContext);
+		 updateAppWidget(appContext, manager, R.layout.main);
+		 
+		 super.onEnabled(context);
+	 }
+	    
+	 @Override
+	 public void onDisabled(Context context) {
+		 context.getApplicationContext().unregisterReceiver(mTimeChangedReceiver);
+		 super.onDisabled(context);
+	 }
+    
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+    	context.getApplicationContext().unregisterReceiver(mTimeChangedReceiver);
+        super.onDeleted(context, appWidgetIds);
+    }
+    
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    	Log.d(TAG, "onUpdate");
+    	updateAppWidget(context, appWidgetManager, R.layout.main);
+    	super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+	public void onReceive(Context context, Intent intent) {
+    	final String action = intent.getAction();
+				
+		if (action.equals(AppWidgetManager.ACTION_APPWIDGET_DELETED)) {
+			final int appWidgetId = intent.getExtras().getInt( AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+				this.onDeleted(context, new int[] { appWidgetId });
+			}
+		}
+		else {
+			super.onReceive(context, intent);
+		}
+
+
+		Log.d(TAG, "intent=" + intent.toString() + "; action="+action);
+		
+		if (action.equals(android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+			Log.d(TAG, "Updating onReceive..." + intent.getComponent().toString());
+			
+			AppWidgetManager appWM = AppWidgetManager.getInstance(context);
+			int[] appWidgetIds = appWM.getAppWidgetIds(intent.getComponent());
+			for (int appWidgetId : appWidgetIds) {
+				updateAppWidget(context, appWM, appWidgetId);
+			}
+		}
+	}
+    
 	public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         RemoteViews views = buildAppWidget(context, appWidgetManager, appWidgetId);
         if (views != null) appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -62,44 +111,15 @@ public class WordClockReceiver extends AppWidgetProvider {
         
         views.setTextColor(R.id.wordclock, Color.parseColor(FontColor));
         
-        Intent intent = new Intent(android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE, Uri.EMPTY, context, WordClockReceiver.class);
+        Intent intent = new Intent(context, Preferences.class);
         PendingIntent pendingIntent;
-
+        
         pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        views.setOnClickPendingIntent(views.getLayoutId(), pendingIntent);
+        views.setOnClickPendingIntent(R.id.wordclock, pendingIntent);
         
         return views;
     }
-
-    @Override
-	public void onReceive(Context context, Intent intent) {
-    	final String action = intent.getAction();
-				
-		if (action.equals(AppWidgetManager.ACTION_APPWIDGET_DELETED)) {
-			final int appWidgetId = intent.getExtras().getInt( AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
-				Log.d(TAG, "Deleting...");
-				this.onDeleted(context, new int[] { appWidgetId });
-			}
-		}
-		else {
-			Log.d(TAG, "onReceive started");
-			super.onReceive(context, intent);
-		}
-
-
-		Log.d(TAG, "intent=" + intent.toString() + "; action="+action);
-		
-		if (action.equals(android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE) || action.equals(android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-			Log.d(TAG, "Updating..." + intent.getComponent().toString());
-			AppWidgetManager appWM = AppWidgetManager.getInstance(context);
-			int[] appWidgetIds = appWM.getAppWidgetIds(intent.getComponent());
-			for (int appWidgetId : appWidgetIds) {
-				updateAppWidget(context, appWM, appWidgetId);
-			}
-		}
-	}
-    
+	
     /**
      * Automatically registered when the Widget is created, and unregistered
      * when the Widget is destroyed.
@@ -109,9 +129,10 @@ public class WordClockReceiver extends AppWidgetProvider {
         public void onReceive(Context context, Intent intent) {
         	Log.d(TAG, "TimeChangedReceiver");
         	
-        	AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        	AppWidgetManager manager = AppWidgetManager.getInstance(context.getApplicationContext());
 			int[] appWidgetIds = manager.getAppWidgetIds(intent.getComponent());
 			for (int appWidgetId : appWidgetIds) {
+				Log.d(TAG, appWidgetId + "");
             	RemoteViews views = buildAppWidget(context, manager, appWidgetId);
             	
                 ComponentName widget = new ComponentName(context.getPackageName(), WordClockReceiver.class.getName());
